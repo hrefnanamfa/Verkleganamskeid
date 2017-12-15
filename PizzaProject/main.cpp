d61a23461b8cf7ee0437aabddb831fec27449e97
Ingi <ingi17@ru.is>
2017-12-15 16:02:52 +0000

    Source Control

--- PizzaProject/main.cpp
+++ PizzaProject/main.cpp
@@ -1,3 +1,24 @@
+/**
+ *  --- Pizza Project ---
+ *
+ *  RU - Reykjavik University
+ *  Verklegt Námskeið 1
+ *  SC-SC-T-113-VLN1
+ *  Hópur 47
+ *
+ *  Afgreiðslukerfi fyrir pizzastað
+ *
+ *
+ * @author  Ingi Þór Aðalsteinsson
+ * @author  Hrefna Namfa Finnsdóttir
+ * @author  Ísak Breki Jónsson
+ *
+ * @version 1.00
+ * @date    15/12/2017
+ *
+ **/
+
+
 #include "include/ui/MainUI.h"
 
 using namespace std;
